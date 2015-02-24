683ed7ef9de5be177a4fc50c39f3dd54f5dde9c9
Jason Coleman <jason@strangerstudios.com>
2015-02-24 13:22:16 -0500

    Added readme. Standardizing plugin names and descriptions.

--- pmpro-add-name-to-checkout.php
+++ pmpro-add-name-to-checkout.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: PMPro Add Name to Checkout
+Plugin Name: Paid Memberships Pro - Add Name to Checkout Add On
 Plugin URI: http://www.paidmembershipspro.com/wp/pmpro-add-name-to-checkout/
 Description: Adds first and last name fields to the user account section at checkout for Paid Memberships Pro.
-Version: .1
+Version: .2
 Author: Stranger Studios
 Author URI: http://www.strangerstudios.com
 */
@@ -11,16 +11,29 @@ Author URI: http://www.strangerstudios.com
 //add the fields to the form
 function pmproan2c_pmpro_checkout_after_password()
 {
-	$first_name = $_REQUEST['first_name'];
-	$last_name = $_REQUEST['last_name']; 
+	global $current_user;
+
+	if(!empty($_REQUEST['first_name']))
+		$first_name = $_REQUEST['first_name'];
+	elseif(!empty($current_user->first_name))
+		$first_name = $current_user->first_name;
+	else
+		$first_name = "";
+		
+	if(!empty($_REQUEST['last_name']))
+		$last_name = $_REQUEST['last_name'];
+	elseif(!empty($current_user->last_name))
+		$last_name = $current_user->last_name;
+	else
+		$last_name = "";
 	?>
 	<div>
 	<label for="first_name">First Name</label>
-	<input id="first_name" name="first_name" type="text" class="input" size="30" value="<?=$first_name?>" />
+	<input id="first_name" name="first_name" type="text" class="input" size="30" value="<?php echo esc_attr($first_name);?>" />
 	</div>
 	<div>
 	<label for="last_name">Last Name</label>
-	<input id="last_name" name="last_name" type="text" class="input" size="30" value="<?=$last_name?>" />
+	<input id="last_name" name="last_name" type="text" class="input" size="30" value="<?php echo esc_attr($last_name);?>" />
 	</div> 
 	<?php
 }
@@ -60,11 +73,16 @@ add_filter("pmpro_registration_checks", "pmproan2c_pmpro_registration_checks");
 
 //update the user after checkout
 function pmproan2c_update_first_and_last_name_after_checkout($user_id)
-{
+{	
+	$current_first_name = get_user_meta($user_id, "first_name", true);
+	$current_last_name = get_user_meta($user_id, "last_name", true);
+	
 	if(!empty($_REQUEST['first_name']))
 		$first_name = $_REQUEST['first_name'];
 	elseif(!empty($_SESSION['first_name']))
 		$first_name = $_SESSION['first_name'];
+	elseif(!empty($current_first_name))
+		$first_name = $current_first_name;
 	else
 		$first_name = "";
 	
@@ -72,13 +90,15 @@ function pmproan2c_update_first_and_last_name_after_checkout($user_id)
 		$last_name = $_REQUEST['last_name'];
 	elseif(!empty($_SESSION['last_name']))
 		$last_name = $_SESSION['last_name'];
+	elseif(!empty($current_last_name))
+		$last_name = $current_last_name;
 	else
 		$last_name = "";
 
 	update_user_meta($user_id, "first_name", $first_name);
-	update_user_meta($user_id, "last_name", $last_name);
+	update_user_meta($user_id, "last_name", $last_name);		
 }
-add_action('pmpro_after_checkout', 'pmproan2c_update_first_and_last_name_after_checkout');
+add_action('pmpro_after_checkout', 'pmproan2c_update_first_and_last_name_after_checkout', 5);
 
 function pmproan2c_pmpro_paypalexpress_session_vars()
 {
@@ -87,3 +107,18 @@ function pmproan2c_pmpro_paypalexpress_session_vars()
 	$_SESSION['last_name'] = $_REQUEST['last_name'];
 }
 add_action("pmpro_paypalexpress_session_vars", "pmproan2c_pmpro_paypalexpress_session_vars");
+
+/*
+Function to add links to the plugin row meta
+*/
+function pmproan2c_plugin_row_meta($links, $file) {
+	if(strpos($file, 'pmpro-add-name-to-checkout.php') !== false)
+	{
+		$new_links = array(
+			'<a href="' . esc_url('http://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
+		);
+		$links = array_merge($links, $new_links);
+	}
+	return $links;
+}
+add_filter('plugin_row_meta', 'pmproan2c_plugin_row_meta', 10, 2);
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== Paid Memberships Pro - Add Name to Checkout Add On ===
+Contributors: strangerstudios
+Tags: paid memberships pro, pmpro, first name, fname, last name, lname, names
+Requires at least: 3.5
+Tested up to: 3.9.1
+Stable tag: .2
+
+Adds first and last name fields to the Paid Memberships Pro checkout page.
+
+== Description ==
+
+Adds first and last name fields to the Paid Memberships Pro checkout page.
+
+== Installation ==
+
+= Download, Install and Activate! =
+1. Upload the `pmpro-add-name-to-checkout` directory to the `/wp-content/plugins/` directory of your site.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= I found a bug in the plugin. =
+
+Please post it in the issues section of GitHub and we'll fix it as soon as we can. Thanks for helping. https://github.com/strangerstudios/pmpro-add-name-to-checkout/issues
+
+= I need help installing, configuring, or customizing the plugin. =
+
+Please visit our premium support site at http://www.paidmembershipspro.com for more documentation and our support forums.
+
+== Changelog ==
+= .2 =
+* Fixed some warnings. Changed priority on pmpro_after_checkout hook to 5 so name meta fields are setup before most other plugins will try to hook.
+* Now also checking if an existing user is logged in with an existing first/last name before potentially overwriting the first/last name on a second checkout.
+
+= .1 =
+* Initial commit.
