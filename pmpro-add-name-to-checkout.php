6ea3d1c4271023e837d96badb925d9623912c663
Mirco Babini <mirkolofio@gmail.com>
2022-04-16 17:25:41 +0200

    Delete session vars after checkout

--- pmpro-add-name-to-checkout.php
+++ pmpro-add-name-to-checkout.php
@@ -199,7 +199,11 @@ function pmproan2c_update_first_and_last_name_after_checkout( $user_id ) {
 
 	update_user_meta( $user_id, 'first_name', $first_name );
 	update_user_meta( $user_id, 'last_name', $last_name );
-	update_post_meta( $user_id, 'billing_name', $first_name.' '.$last_name);
+	update_post_meta( $user_id, 'billing_name', $first_name . ' ' . $last_name );
+
+	// unset the user fields in session
+	unset( $_SESSION['first_name'] );
+	unset( $_SESSION['last_name'] );
 }
 add_action( 'pmpro_after_checkout', 'pmproan2c_update_first_and_last_name_after_checkout' );
 
